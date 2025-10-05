ed4c0de8a12a9d1cefacd358740a48fd5e419298
AnthonyGuerrero56 <u202010054@upc.edu.pe>
2025-10-05 12:40:43 -0500

    style: add global SCSS styles for layout and NG-ZORRO components

--- org-divisions-frontend/src/app/app.config.ts
+++ org-divisions-frontend/src/app/app.config.ts
@@ -1,9 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
+    provideZonelessChangeDetection(), provideNzI18n(en_US), provideAnimationsAsync(), provideHttpClient(),
     
   ]
 };
